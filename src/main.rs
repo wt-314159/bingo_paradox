c17046384d854a98cc16251cd9b43d3886794899
Will Train <87541128+wt-314159@users.noreply.github.com>
2024-08-22 02:45:18 +0100

    Benchmarking 2 different methods of testing for hits on bingo cards

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use std::fmt;
+use std::{fmt, time::Instant};
 use rand::{self, Rng, thread_rng};
+
+const SIZE: usize = 1_000_000;
 
 fn main() {
     let mut test = BingoCard::new();
-    test.matches[0][0] = true;
-    test.matches[2][2] = true;
-    test.matches[1][4] = true;
-    test.matches[0][1] = true;
-    println!();
-    println!("{test}");
+    let mut rng = thread_rng();
+    let mut randoms: [usize; SIZE] = [0; SIZE];
+
+    let now = Instant::now();
+    for i in 0..SIZE {
+        randoms[i] = rng.gen_range(1..=75);
+    }
+    println!("Randoms generated in {:#?}", now.elapsed());
+
+    let mut count = 0;
+    let now = Instant::now();
+    for i in 0..SIZE {
+        if test.contains_number(randoms[i]) {
+            count += 1;
+        }
+    }
+    println!("There were {} matches in {:#?}", count, now.elapsed());
+
+    count = 0;
+    let now = Instant::now();
+    for i in 0..SIZE {
+        if test.find_match(randoms[i]) {
+            count += 1;
+        }
+    }
+    println!("There were {} matches in {:#?}", count, now.elapsed());
 }
 
 struct BingoCard {
@@ -34,6 +56,21 @@ impl BingoCard {
 
         numbers[2][2] = 0;
         BingoCard { numbers, matches: [[false;5];5] }
+    }
+
+    pub fn contains_number(&self, number: usize) -> bool {
+        self.numbers.iter().any(|x| x.contains(&number))
+    }
+
+    pub fn find_match(&self, number: usize) -> bool {
+        for i in 0..5 {
+            for j in 0..5 {
+                if self.numbers[i][j] == number {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
